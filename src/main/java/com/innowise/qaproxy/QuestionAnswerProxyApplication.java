5d4eb6eabd2a71722f0e21ebf78a64e7408482cb
Yury Lapitski /VikaNoteBook_Win11 <yury.connect@gmail.com>
2026-09-02 12:48:37 +0300

    init commit

---
+++ src/main/java/com/innowise/qaproxy/QuestionAnswerProxyApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.qaproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionAnswerProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionAnswerProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innowise/qaproxy/QuestionAnswerProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innowise.qaproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionAnswerProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
